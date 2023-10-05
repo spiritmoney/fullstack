1784c5b3ddcfcde6c6aa128828b2760c6e5e5521
King Odim <kingsleyodim@gmail.com>
2023-10-05 18:15:18 +0100

    1.3-1.5

---
+++ index.js
@@ -0,0 +1,3 @@
+const t = [1, 2, 3];
+const tSquared = t.map(x => x * x);
+console.log(tSquared);
--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,76 +1,34 @@
-const Header = (props) => {
-  return (
-    <div>
-      <p> {props.course}</p>
-    </div>
-  )
-}
-
-const Part1 = () => {
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-
-  return (
-    <div>
-      <p>{part1}{exercises1}</p>
-    </div>
-  )
-}
-
-const Part2 = () => {
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-
-  return (
-    <div>
-      <p>{part2}{exercises2}</p>
-    </div>
-  )
-}
-
-const Part3 = () => {
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
-  return (
-    <div>
-      <p>{part3}{exercises3}</p>
-    </div>
-  )
-}
-
-const Content = () => {
-  return (
-    <div>
-      <Part1 />
-      <Part2 />
-      <Part3 />
-    </div>
-  )
-}
-
-const Total = () => {
-  const exercises1 = 10
-  const exercises2 = 7
-  const exercises3 = 14
-
-  return (
-    <div>
-      <p>Total Number of exercises {exercises1 + exercises2 + exercises3}</p>
-    </div>
-  )
-}
-
 const App = () => {
-  const course = 'Half Stack application development'
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  };
 
   return (
     <div>
-      <h1><Header course={course} /></h1>
-      <Content />
-      <Total />
+      <h1>{course.name}</h1>
+      <ul>
+        {course.parts.map((part, index) => (
+          <li key={index}>
+            {part.name} - {part.exercises} exercises
+          </li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
 export default App
